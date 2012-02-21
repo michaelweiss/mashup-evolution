b1de081a6c30d97776fd99d6a017efb81de1e11d
Michael Weiss <weiss@sce.carleton.ca>
2012-02-21 12:48:31 -0500

    plot actual vs simulated word distribution

--- bentley/words.R
+++ bentley/words.R
@@ -1,10 +1,13 @@
 words <- read.table("words.csv", header=TRUE)
-plot(abs ~ rank, data=words, log="xy", col="red")
+plot(abs ~ rank, data=words, log="xy", col="red", xlab="Rank", ylab="Size")
 
 bentley.words <- read.table("bentley_1000_0004_4_450_8000.csv", header=FALSE)
 names(bentley.words) <- c("location", "size")
+top.1000 <- sort(bentley.words$size, decreasing=T)[1:1000]
+points(top.1000)
 
-bentley.words.freq <- table(bentley.words$size)
-bentley.words.freq.m <- as.matrix(bentley.words.freq)
+alpha.words <- power.law.fit(words$abs)
+# lines(1:100, words$abs[1]*(1:100)^(-coef(alpha.words)+1), col="red")
 
-plot(bentley.words.freq.m, log="xy", xlab="size", ylab="P(size)")
+alpha.bentley.words <- power.law.fit(top.1000)
+# lines(1:100, top.1000[1]*(1:100)^(-coef(alpha.bentley.words)+1), col="black")
