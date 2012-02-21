907e91c5ac8f1db374378c28d3cec2d6ff027660
Michael Weiss <weiss@sce.carleton.ca>
2012-02-20 21:39:11 -0500

    initial support for memory

--- bentley/bentley.pl
+++ bentley/bentley.pl
@@ -1,22 +1,25 @@
 #!/usr/bin/perl
 
-my $DEBUG = 0;				# show debug messages
+my $DEBUG = 1;				# show debug messages
 
 my $N = 1;					# initial number of agents
-my $n = 10;					# number of agents joining each step
+my $n = 3;					# number of agents joining each step
 my $mu = 0.6;				# innovation parameter
-my $m = -1;					# number of previous steps (-1 is all)
+my $m = 1;					# number of previous steps (-1 is all)
 my $t = 0;					# time step
-my $T = 20000;				# length of simulation in time steps
+my $T = 5;					 # length of simulation in time steps
 	
 my @location;				# locations of agents
 my $l = $N;					# next available new location
+
+my @memory;					# most $m*$n recent choices
 
 my $seed = time() + $$;		# time + process id
 
 sub init {
 	srand($seed);
 	foreach $j (0..$N-1) {
+		addLocation($j);
 		$location[$j] = 1;
 	}
 }
@@ -31,11 +34,13 @@ sub grow {
 				# the agent copies his location from another agent within the previous m=all steps
 				my $k;		# location selected for copying
 				$k = selectLocation($t);
+				addLocation($k);
 				print "k = $k\n" if ($DEBUG);
 				$location[$k]++;
 			} else {
 				# with probability $mu
 				# the agent innovates by choosing a new location at random		
+				addLocation($l);
 				print "k = $l (innovation)\n" if ($DEBUG);
 				$location[$l++] = 1;
 			}
@@ -46,27 +51,39 @@ sub grow {
 
 sub selectLocation {
 	my ($t) = @_;
-	my $r = int(rand()*($N+($t-1)*$n));
-	print "r = $r\n" if ($DEBUG);
-	my $k = findLocationInterval($r);
-	return $k;
+	# initial implementation of memory
+	# select a choice at random
+	# this implementation makes an incorrect simplificiation:
+	# should only select location from the $m previous time steps
+	print "memory at t = $t\n" if ($DEBUG);
+	showMemory() if ($DEBUG);
+	my $r = int(rand()*$#memory);
+	return $memory[$r];
 }
 
-sub findLocationInterval {
-	my ($r) = @_;
-	my $s = 0;
-	foreach $j (0..$l-1) {
-		$s += $location[$j];
-		if ($r < $s) {
-			return $j;
-		}
+sub addLocation {
+	my ($location) = @_;
+	# initial implementation of memory
+	# add new location to the end of the memory
+	# remove oldest location from the front
+	# this implementation makes an incorrect simplificiation:
+	# locations should only be copied from the $m previous time steps
+	# so should only remove old locations at end of time step
+	push(@memory, $location);
+	if ($#memory == $m*$n) {	# $m*$n is one more than allowed
+		shift @memory;
 	}
-	die "did not find a location: r = $r";
 }
 
 sub show {
 	foreach $j (0..$l-1) {
 		print "$j, $location[$j]\n";
+	}
+}
+
+sub showMemory {
+	foreach $j (0..$m*$n-1) {
+		print "$j, $memory[$j]\n";
 	}
 }
 
